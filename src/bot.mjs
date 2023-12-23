aa8e3da7bf4d1d33113fa099d618ffc0e6f5e08f
Zoom <99123419+Zoomish@users.noreply.github.com>
2023-12-23 17:18:22 +0300

    init

--- src/bot.mjs
+++ src/bot.mjs
@@ -3,10 +3,8 @@ import TeleBot from "telebot"
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 const WebUrlit='https://rococo-wisp-b5b1a7.netlify.app/';
 
+bot.on('/start', msg => {
 
-bot.on('/start', async (msg) => {
-    const chatId = msg.from.id;
-    const text=msg.text;
     let replyMarkup = bot.inlineKeyboard([
         [
             bot.inlineButton('callback', {callback: 'this_is_data'}),
@@ -16,7 +14,13 @@ bot.on('/start', async (msg) => {
         ]
     ]);
 
+    return bot.sendMessage(msg.from.id, 'Down', {replyMarkup});
 
+});
+
+bot.on('/start', async (msg) => {
+    const chatId = msg.from.id;
+    const text=msg.text;
         await bot.sendMessage(chatId,'Check up:', {
             reply_markup:{
                 inline_keyboard:[
@@ -45,5 +49,77 @@ bot.on('/start', async (msg) => {
     }
 });
 
+bot.on(['/start', '/back'], msg => {
 
+    let replyMarkup = bot.keyboard([
+        ['/buttons', '/inlineKeyboard'],
+        ['/start', '/hide']
+    ], {resize: true});
+
+    return bot.sendMessage(msg.from.id, 'Keyboard example.', {replyMarkup});
+
+});
+
+// Buttons
+bot.on('/buttons', msg => {
+
+    let replyMarkup = bot.keyboard([
+        [bot.button('contact', 'Your contact'), bot.button('location', 'Your location')],
+        ['/back', '/hide']
+    ], {resize: true});
+
+    return bot.sendMessage(msg.from.id, 'Button example.', {replyMarkup});
+
+});
+
+// Hide keyboard
+bot.on('/hide', msg => {
+    return bot.sendMessage(
+        msg.from.id, 'Hide keyboard example. Type /back to show.', {replyMarkup: 'hide'}
+    );
+});
+
+// On location on contact message
+bot.on(['location', 'contact'], (msg, self) => {
+    return bot.sendMessage(msg.from.id, `Thank you for ${ self.type }.`);
+});
+
+// Inline buttons
+bot.on('/inlineKeyboard', msg => {
+
+    let replyMarkup = bot.inlineKeyboard([
+        [
+            bot.inlineButton('callback', {callback: 'this_is_data'}),
+            bot.inlineButton('inline', {inline: 'some query'})
+        ], [
+            bot.inlineButton('url', {url: 'https://telegram.org'})
+        ]
+    ]);
+
+    return bot.sendMessage(msg.from.id, 'Inline keyboard example.', {replyMarkup});
+
+});
+
+// Inline button callback
+bot.on('callbackQuery', msg => {
+    // User message alert
+    return bot.answerCallbackQuery(msg.id, `Inline button callback: ${ msg.data }`, true);
+});
+
+// Inline query
+bot.on('inlineQuery', msg => {
+
+    const query = msg.query;
+    const answers = bot.answerList(msg.id);
+
+    answers.addArticle({
+        id: 'query',
+        title: 'Inline Query',
+        description: `Your query: ${ query }`,
+        message_text: 'Click!'
+    });
+
+    return bot.answerQuery(answers);
+
+});
 export default bot
