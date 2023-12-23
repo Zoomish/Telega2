6eb312435a2e6921648950227e5fdcaa635d9d8a
Zoom <99123419+Zoomish@users.noreply.github.com>
2023-12-23 17:59:36 +0300

    init

--- src/bot.mjs
+++ src/bot.mjs
@@ -2,8 +2,10 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 const WebUrlit='https://rococo-wisp-b5b1a7.netlify.app/';
+const chatId = msg.from.id;
+const text=msg.text;
 
-bot.on('/start', msg => {
+bot.on('/form', msg => {
 
     let replyMarkup = bot.keyboard([
         [bot.button('form', {url: WebUrl + 'form'})]
@@ -17,28 +19,27 @@ bot.on('/start', msg => {
 
 bot.on('/start', async (msg) => {
     let replyMarkup = bot.inlineKeyboard([[bot.inlineButton('url', {url: 'https://rococo-wisp-b5b1a7.netlify.app/'})]]);
-    const chatId = msg.from.id;
-    const text=msg.text;
+    
     await bot.sendMessage(msg.from.id, 'Check up:', {replyMarkup});
 
-    if (msg?.web_app_data?.data) {
-        try {
-            const data=JSON.parse(msg?.web_app_data?.data)
+    // if (msg?.web_app_data?.data) {
+    //     try {
+    //         const data=JSON.parse(msg?.web_app_data?.data)
 
-            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country)
-            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street)
-            await bot.sendMessage(chatId, 'Спасибо за форму!')
+    //         await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country)
+    //         await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street)
+    //         await bot.sendMessage(chatId, 'Спасибо за форму!')
 
-            setTimeout(async ()=>{
-                await bot.sendMessage(chatId,'Всю  ифнормацию можно получить по... Никак!');
-            }, 3000)
+    //         setTimeout(async ()=>{
+    //             await bot.sendMessage(chatId,'Всю  ифнормацию можно получить по... Никак!');
+    //         }, 3000)
 
-        }catch(e){
-            console.log(e);
-        }
+    //     }catch(e){
+    //         console.log(e);
+    //     }
 
         
-    }
+    // }
 });
 
 
