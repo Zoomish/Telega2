0e48f5f431a1e55e90e838d525d519882dcb87e5
Zoom <99123419+Zoomish@users.noreply.github.com>
2023-12-23 16:20:23 +0300

    init

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,18 @@
 import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const WebUrl='https://rococo-wisp-b5b1a7.netlify.app/';
 
-bot.on("text", msg => msg.reply.text(msg.text))
+bot.on('/hello', (msg) => {
+    return bot.sendMessage(msg.from.id, `Hello, ${msg.from.first_name}!`);
+  });
+bot.on(['/start', 'audio', 'sticker'], msg => {
+  return bot.sendMessage(msg.from.id, 'Bam!');
+});
+bot.on('/start', async msg => {
+    await bot.sendMessage(chatId,'Down')
 
+
+    await bot.sendMessage(chatId,'Check up:',)
+});
 export default bot
