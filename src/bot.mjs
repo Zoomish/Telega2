71555383f0c6e1fb2b29248b434a9bf4076556c7
Zoom <99123419+Zoomish@users.noreply.github.com>
2023-12-23 17:36:44 +0300

    init

--- src/bot.mjs
+++ src/bot.mjs
@@ -3,15 +3,14 @@ import TeleBot from "telebot"
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 const WebUrlit='https://rococo-wisp-b5b1a7.netlify.app/';
 
-
 bot.on('/start', async (msg) => {
-    const replyMarkup = bot.inlineKeyboard([[bot.inlineButton('url', {url: 'https://rococo-wisp-b5b1a7.netlify.app/'})]]);
+    let replyMarkup = bot.inlineKeyboard([[bot.inlineButton('url', {url: 'https://rococo-wisp-b5b1a7.netlify.app/'})]]);
     const chatId = msg.from.id;
     const text=msg.text;
     await bot.sendMessage(chatId,'Down', {
         reply_markup:{
             keyboard:[
-                [{text:'Заполнить форму', web_app: {url: WebUrlit + 'form'}}]
+                [{text:'Заполнить форму', web_app: {url: WebUrl + 'form'}}]
             ]
         }
     })
