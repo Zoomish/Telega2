cd1d873f9edaac1c98e34bc1c5279322b5572420
Zoom <99123419+Zoomish@users.noreply.github.com>
2023-12-25 21:41:18 +0300

    init

--- src/bot.mjs
+++ src/bot.mjs
@@ -18,35 +18,44 @@ bot.on(['location', 'contact'], (msg, self) => {
     return bot.sendMessage(msg.from.id, `Thank you for ${ self.type }.`);
 });
 
+bot.on(['/start', '/back'], msg => {
 
-bot.on('/start', async (msg) => {
-    const chatId = msg.from.id;
+    let replyMarkup = bot.keyboard([
+        ['/buttons', '/inlineKeyboard'],
+        ['/start', '/hide']
+    ], {resize: true});
 
-    // await bot.sendMessage(msg.from.id,bot.keyboard([['form']], {resize: true}))
+    return bot.sendMessage(msg.from.id, 'Keyboard example.', {replyMarkup});
 
-    let replyMarkup = bot.inlineKeyboard([[bot.inlineButton('url', {url: 'https://rococo-wisp-b5b1a7.netlify.app/'})]]);
+});
+// bot.on('/start', async (msg) => {
+//     const chatId = msg.from.id;
+
+//     // await bot.sendMessage(msg.from.id,bot.keyboard([['form']], {resize: true}))
+
+//     let replyMarkup = bot.inlineKeyboard([[bot.inlineButton('url', {url: 'https://rococo-wisp-b5b1a7.netlify.app/'})]]);
     
-    await bot.sendMessage(msg.from.id, 'Check up:', {replyMarkup});
+//     await bot.sendMessage(msg.from.id, 'Check up:', {replyMarkup});
 
-    // if (msg?.web_app_data?.data) {
-    //     try {
-    //         const data=JSON.parse(msg?.web_app_data?.data)
+//     // if (msg?.web_app_data?.data) {
+//     //     try {
+//     //         const data=JSON.parse(msg?.web_app_data?.data)
 
-    //         await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country)
-    //         await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street)
-    //         await bot.sendMessage(chatId, 'Спасибо за форму!')
+//     //         await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country)
+//     //         await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street)
+//     //         await bot.sendMessage(chatId, 'Спасибо за форму!')
 
-    //         setTimeout(async ()=>{
-    //             await bot.sendMessage(chatId,'Всю  ифнормацию можно получить по... Никак!');
-    //         }, 3000)
+//     //         setTimeout(async ()=>{
+//     //             await bot.sendMessage(chatId,'Всю  ифнормацию можно получить по... Никак!');
+//     //         }, 3000)
 
-    //     }catch(e){
-    //         console.log(e);
-    //     }
+//     //     }catch(e){
+//     //         console.log(e);
+//     //     }
 
         
-    // }
-});
+//     // }
+// });
 
 
 export default bot
